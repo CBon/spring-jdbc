2db712eb4aff853195d230aef9982a762b10d580
Constantine Bondarenko <konstantin.bondarenko@rtlabs.ru>
2022-10-16 10:43:27 +0300

    fix problems with datasource and autoGeneratedKeys

--- src/main/java/com/baeldung/spring/jdbc/autogenkey/repository/MessageRepositoryJDBCTemplate.java
+++ src/main/java/com/baeldung/spring/jdbc/autogenkey/repository/MessageRepositoryJDBCTemplate.java
@@ -1,36 +1,35 @@
 package com.baeldung.spring.jdbc.autogenkey.repository;
 
-import java.sql.PreparedStatement;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Optional;
+
 @Repository
 public class MessageRepositoryJDBCTemplate {
 
+    static final String INSERT_MESSAGE_SQL = "insert into sys_message (message) values(?) ";
+    static final String SELECT_BY_ID = "select message from sys_message where id = ?";
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    static final String INSERT_MESSAGE_SQL = "insert into sys_message (message) values(?) ";
-
     public long insert(final String message) {
-        
+
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
         jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(INSERT_MESSAGE_SQL);
+            PreparedStatement ps = connection.prepareStatement(INSERT_MESSAGE_SQL, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, message);
             return ps;
         }, keyHolder);
 
-        return Optional.ofNullable(keyHolder.getKey()).map(long.class::cast).orElse(0L);
+        return Optional.ofNullable(keyHolder.getKey()).map(Number::longValue).orElse(0L);
     }
-
-    static final String SELECT_BY_ID = "select message from sys_message where id = ?";
 
     public String getMessageById(long id) {
         return this.jdbcTemplate.queryForObject(SELECT_BY_ID, String.class, id);
